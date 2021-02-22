775d3465a6d1b0ba58fdd5c78f9c7876081d8451
Simone Picciau <simone.picciau@gmail.com>
2021-02-22 11:48:12 +0100

    terraform support

---
+++ spark-terraform-master/variables.tf
@@ -0,0 +1,70 @@
+variable "region" {
+    type = string
+    default = "us-east-2" # Stati Uniti Orientali (Ohio)
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro" # free tier
+}
+
+variable "ami_image" {
+    type = string
+    default = "ami-0996d3051b72b5b2c" # ubuntu 20.04
+}
+
+variable "profile"{
+    type = string
+    default = "default"
+}
+
+variable "key_name" {
+    type = string
+    default = "localkey"
+}
+
+variable "nomeChiaveAws" {
+    type = string
+    default = "chiave_aws"
+}
+
+variable "pathChiaveAws" {
+    type = string
+    default = "chiave_aws.pem"
+}
+
+variable "numOfMasters" {
+    type = number
+    default = 1
+}
+
+variable "numOfSlaves" {
+    type = number
+    default = 1
+}
+
+variable "mgmt_jump_private_ips_master" {
+    default = "172.31.32.101"
+}
+
+variable "mgmt_jump_private_ips" {
+    default = {
+        "0" = "172.31.32.102"
+        "1" = "172.31.32.103"
+        "2" = "172.31.32.104"
+        "3" = "172.31.32.105"
+        "4" = "172.31.32.106"
+        "5" = "172.31.32.107"
+    }
+}
+
+variable "mgmt_jump_hostnames" {
+    default = {
+        "0" = "slave_1"
+        "1" = "slave_2"
+        "2" = "slave_3"
+        "3" = "slave_4"
+        "4" = "slave_5"
+        "5" = "slave_6"
+    }
+}
