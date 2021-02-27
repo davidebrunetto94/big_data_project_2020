8dfcc3b141497d277e20fbd4408487a40f9984ef
davidebrunetto94 <37103188+davidebrunetto94@users.noreply.github.com>
2021-02-27 09:06:50 +0100

    uncommented lines

--- proj/trend_graphs.py
+++ proj/trend_graphs.py
@@ -23,16 +23,16 @@ f1.clear()
 plt.close(f1)
 
 
-# # Tweet volume graph
-# count_df = get_tweet_count_df()
-# x = count_df.select('timestamp').rdd.map(
-#     lambda x: datetime.datetime.strftime(x[0], "%d/%m/%Y")).collect()
-# y = count_df.select('count').rdd.map(lambda x: x[0]).collect()
-# #ax2 = plt.gca()
-# f2 = plt.figure()
-# plt.xticks(rotation=90, fontsize=7)
-# plt.xlabel('dates', fontsize=5)
-# plt.plot(x, y)
-# plt.savefig('volume_trend_graph.png', bbox_inches="tight")
-# f2.clear()
-# plt.close(f2)
+# Tweet volume graph
+count_df = get_tweet_count_df()
+x = count_df.select('timestamp').rdd.map(
+    lambda x: datetime.datetime.strftime(x[0], "%d/%m/%Y")).collect()
+y = count_df.select('count').rdd.map(lambda x: x[0]).collect()
+#ax2 = plt.gca()
+f2 = plt.figure()
+plt.xticks(rotation=90, fontsize=7)
+plt.xlabel('dates', fontsize=5)
+plt.plot(x, y)
+plt.savefig('volume_trend_graph.png', bbox_inches="tight")
+f2.clear()
+plt.close(f2)
