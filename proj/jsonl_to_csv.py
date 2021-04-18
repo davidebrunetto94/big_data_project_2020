bcaa8e30783bf5eb4ea846cf6eca9f68df4ae9c0
davidebrunetto94 <37103188+davidebrunetto94@users.noreply.github.com>
2021-04-18 13:24:16 +0200

    Improved jsonl_to_csv

--- proj/jsonl_to_csv.py
+++ proj/jsonl_to_csv.py
@@ -2,13 +2,15 @@ import findspark
 findspark.init()
 findspark.find()
 import pyspark
-findspark.find()
 import os
 import re
-
+import shutil
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StringType, FloatType, StructField
+
+findspark.init()
+findspark.find()
 
 #   Initialize the Spark session
 conf = pyspark.SparkConf().setAppName('appName').setMaster('local')
@@ -17,25 +19,29 @@ sc = pyspark.SparkContext(conf=conf)
 #Imports for pysparksql
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import *
-
-source_path = r"C:\Users\Davide\id_tweets\hydrated_tweets_01.jsonl"
 sqlContext = SQLContext(sc)
 
-df = sqlContext.read.json(source_path)
-df_small = df.select('full_text', 'id_str', 'created_at')
-
-df_small.coalesce(1).write.option("header", "true").option("delimiter", "\t").csv("name.csv")
-
 dim_dataset = 30
+#If you launch the script in the directory with the files, you can leave this as it is, otherwise you have to specify the path
+source_path = r"./"
+
 for i in range(1, dim_dataset+1):
     if i < 10:
         ind = '0' + str(i)
     else:
         ind = str(i)
-    source_path = r"C:\Users\Davide\id_tweets\hydrated_tweets_" + ind + ".csv"
-    for filename in os.listdir(source_path):
+    source_file = source_path + "hydrated_tweets_" + ind + ".jsonl"
+    df = sqlContext.read.json(source_file)
+    #create a new df with only the selected fields
+    df_small = df.select('full_text', 'id_str', 'created_at')
+    folder_name = "name" + ind + ".csv"
+    #write them
+    df_small.coalesce(1).write.option("header", "true").option("delimiter", "\t").csv(folder_name)
+    target_folder = os.path.join(source_path,folder_name)
+    for filename in os.listdir(target_folder):
         if re.match("part-.+[.csv]$", filename):
-            print("match")
-            new_path = r"C:\Users\Davide\id_tweets\hydrated_tweets_csv"
-            new_name = r"hydrated_tweets_" + ind + ".csv"
-            os.rename(os.path.join(source_path,filename), os.path.join(new_path, new_name))
+            print("-----------------MATCHHHHHH----------")
+            new_path = r"./"
+            new_name = "hydrated_tweets_" + ind + ".csv"
+            os.rename(os.path.join(target_folder,filename), os.path.join(new_path, new_name))
+            shutil.rmtree(target_folder, ignore_errors=True)
